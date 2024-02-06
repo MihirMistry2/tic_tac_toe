d182f29950910e700dd40c2733be2c12ef5f6936
MihirMistry2 <mihirmistry82@gmail.com>
2024-02-06 07:48:18 +0530

    Restart game functionality has been added to the Play again component.

--- src/components/PlayAgain/index.js
+++ src/components/PlayAgain/index.js
@@ -1,9 +1,22 @@
 import { motion } from 'framer-motion';
 
-const PlayAgain = () => {
+/**
+ * This component renders the 'Play Again' button, on click game restart.
+ * @param {Function} setItems Set items 'x' or 'o' state.
+ * @returns {React.ReactElement} React element that renders a search input with a button.
+ */
+const PlayAgain = ({ setItems }) => {
+    /**
+     * On click restart game state.
+     * @param {Event} e 
+     */
+    const onButtonClick = (e) => {
+        setItems(Array(9).fill(''));
+    };
+
     return (
         <div className="play-again-wrapper">
-            <motion.div className="play-again-btn" whileHover={{ scale: 1.05 }} whileTap={{ scale: 1 }}>
+            <motion.div className="play-again-btn" whileHover={{ scale: 1.05 }} whileTap={{ scale: 1 }} onClick={onButtonClick}>
                 <span className="play-again-title">play again</span>
                 <span className="play-again-icon"></span>
             </motion.div>
