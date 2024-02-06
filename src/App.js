b1af950e0c73cece5c78f86a00d2c18f3e09b52f
MihirMistry2 <mihirmistry82@gmail.com>
2024-02-06 07:50:56 +0530

    Grid square component added for rendering the player's 'X' or 'O' state.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import PlayAgain from './components/PlayAgain/index';
+import { useState } from 'react';
+import PlayAgain from './components/PlayAgain';
+import Square from './components/Square';
 import './sass/App.scss';
 
 const App = () => {
+    /** @type {[items: string[], setItems: Function]}  */
+    const [items, setItems] = useState(Array(9).fill(''));
+    /** @type {[turn: string, setTurn: Function]} */
+    const [turn, setTurn] = useState('x');
+
     return (
         <div className="main-wrapper">
             <div className="title-wrapper">
                 <h1>tic tac toe</h1>
             </div>
             <div className="game-container">
-                <PlayAgain />
+                <PlayAgain setItems={setItems} />
+                <Square items={items} setItems={setItems} turn={turn} setTurn={setTurn}/>
             </div>
         </div>
     );
---
+++ src/components/Square/index.js
@@ -0,0 +1,54 @@
+import { motion } from 'framer-motion';
+import XIcon from '../../assets/icons/x.svg';
+import OIcon from '../../assets/icons/o.svg';
+
+/**
+ * This component creates a grid container that is used for rendering 'x' or 'o'.
+ * @param {string[]} items Player's selected values.
+ * @param {Function} setItems Set items 'x' or 'o' state.
+ * @param {string} turn Player's turn state.
+ * @param {Function} setTurn Set Player's turn state.
+ * @returns {React.ReactElement} React element that renders a search input with a button.
+ */
+const Square = ({ items, setItems, turn, setTurn }) => {
+    const parentVariant = {
+        initial: { opacity: 0 },
+        animate: {
+            opacity: 1,
+            scale: 1,
+            transition: { delayChildren: 0.1, staggerChildren: 0.2 },
+        },
+    };
+    const childrenVariant = {
+        initial: { opacity: 0, scale: 0 },
+        animate: { opacity: 1, scale: 1 },
+    };
+
+    /**
+     * To update the player's turn state and display an 'X' or 'O' icon.
+     * @param {Evant} e
+     */
+    const onItemClick = (e) => {
+        try {
+            const $target = e.currentTarget;
+            const index = Number($target.id);
+            items[index] = turn;
+            setItems([...items]);
+            setTurn((prev) => (prev === 'x' ? 'o' : 'x'));
+        } catch (error) {
+            alert(`Error: ${error.message}`);
+        }
+    };
+
+    return (
+        <motion.div className="grid-container" initial="initial" animate="animate" variants={parentVariant}>
+            {items.map((item, i) => (
+                <motion.div key={i} id={i} className="grid-item" variants={childrenVariant} onClick={onItemClick}>
+                    {item === 'x' ? <motion.img initial={{ scale: 0 }} animate={{ scale: 1 }} src={XIcon} alt="x" width="80px" height="80px"/> : null}
+                    {item === 'o' ? <motion.img initial={{ scale: 0 }} animate={{ scale: 1 }} src={OIcon} alt="o" width="60px" height="60px"/> : null}
+                </motion.div>
+            ))}
+        </motion.div>
+    );
+};
+export default Square;
